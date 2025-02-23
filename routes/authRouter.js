7a4dce6bd0b69dde1166fa5099e826023c3361b1
Srivikas Varasala <128049761+Srivikas2004@users.noreply.github.com>
2025-02-23 19:12:28 +0530

    Update authRouter.js

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -6,7 +6,7 @@ router.get('/test',(req,res)=>{
     res.send('test pass');
 })
 
-router.get('/google',googleLogin);
+router.get('/google/callback',googleLogin);
 router.post("/login", normalLogin);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password",resetPassword);
